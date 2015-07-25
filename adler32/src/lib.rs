d3c2872029b451384d9b879b257548c8e830ad66
Remi Rampin <remirampin@gmail.com>
2015-07-25 19:29:47 -0400

    Remove test comparing to reference

--- adler32/src/lib.rs
+++ adler32/src/lib.rs
@@ -1,7 +1,6 @@
 extern crate rand;
 
 use std::io;
-use rand::Rng;
 
 const MOD_ADLER: u32 = 65521;
 
@@ -21,34 +20,28 @@ pub fn adler32<R: io::Read>(reader: R) -> io::Result<u32> {
     Ok((b << 16) | a)
 }
 
-use self::adler32 as adler32_default;
+#[cfg(test)]
+mod test {
+    use rand::Rng;
+    use std::io::Cursor;
 
-#[test]
-fn testvectors() {
-    fn do_test(v: u32, bytes: &[u8]) {
-        let r = io::Cursor::new(bytes);
-        assert_eq!(adler32(r).unwrap(), v);
-    }
-    do_test(0x00000001, b"");
-    do_test(0x00620062, b"a");
-    do_test(0x024d0127, b"abc");
-    do_test(0x29750586, b"message digest");
-    do_test(0x90860b20, b"abcdefghijklmnopqrstuvwxyz");
-    do_test(0x8adb150c, b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                          abcdefghijklmnopqrstuvwxyz\
-                          0123456789");
-    do_test(0x97b61069, b"1234567890123456789012345678901234567890\
-                          1234567890123456789012345678901234567890");
-}
+    use super::adler32;
 
-#[test]
-fn compare() {
-    let mut rng = rand::thread_rng();
-    let mut data = vec![0u8; 128];
-    for size in [0, 1, 3, 4, 5, 31, 32, 33, 67, 126].iter().cloned() {
-        rng.fill_bytes(&mut data[..size]);
-        let r1 = io::Cursor::new(&data[..size]);
-        let r2 = r1.clone();
-        assert_eq!(adler32_default(r1).unwrap(), adler32(r2).unwrap());
+    #[test]
+    fn testvectors() {
+        fn do_test(v: u32, bytes: &[u8]) {
+            let r = Cursor::new(bytes);
+            assert_eq!(adler32(r).unwrap(), v);
+        }
+        do_test(0x00000001, b"");
+        do_test(0x00620062, b"a");
+        do_test(0x024d0127, b"abc");
+        do_test(0x29750586, b"message digest");
+        do_test(0x90860b20, b"abcdefghijklmnopqrstuvwxyz");
+        do_test(0x8adb150c, b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                              abcdefghijklmnopqrstuvwxyz\
+                              0123456789");
+        do_test(0x97b61069, b"1234567890123456789012345678901234567890\
+                              1234567890123456789012345678901234567890");
     }
 }
