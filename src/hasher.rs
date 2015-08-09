548fdb6ae63ebc463343bc135cdecd5d6964f41c
Remi Rampin <remirampin@gmail.com>
2015-08-09 10:58:52 -0400

    Implement default hashes: Adler32 + SHA-1

--- src/hasher.rs
+++ src/hasher.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
+use std::default::Default;
 use std::hash::Hash;
 use std::io;
+
+use adler32::adler32;
+use sha1::Sha1;
 
 pub struct BlockLocation<F> {
     pub file: F,
@@ -47,5 +51,35 @@ impl<F, H, HF: Fn(&[u8]) -> H> Hashes<F, H, HF> where F: Clone, H: Eq + Hash {
 
     pub fn find(&self, hash: &H) -> Option<&BlockLocation<F>> {
         self.blocks.get(hash)
+    }
+}
+
+/// Default hashes, used in rs-sync: Adler32 and SHA-1
+#[derive(PartialEq, Eq, Hash, Clone)]
+pub struct Adler32_SHA1 {
+    pub adler32: u32,
+    pub sha1: [u8; 20],
+}
+
+/// Computes the default hashes, used in rs-sync: Adler32 and SHA-1
+pub fn adler32_sha1(block: &[u8]) -> Adler32_SHA1 {
+    let adler32 = adler32(block).unwrap();
+    let sha1 = {
+        let mut sha1 = Sha1::new();
+        sha1.update(block);
+        let mut digest: [u8; 20] = unsafe { ::std::mem::uninitialized() };
+        sha1.output(&mut digest);
+        digest
+    };
+    Adler32_SHA1 { adler32: adler32,
+                   sha1: sha1 }
+}
+
+pub type DefaultHashes = Hashes<::std::path::PathBuf, Adler32_SHA1,
+                                fn(&[u8]) -> Adler32_SHA1>;
+
+impl Default for DefaultHashes {
+    fn default() -> DefaultHashes {
+        DefaultHashes::new(adler32_sha1)
     }
 }
