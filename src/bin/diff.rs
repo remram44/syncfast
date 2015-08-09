e21b542486a63b778efe912bc0464709ebc699f5
Remi Rampin <remirampin@gmail.com>
2015-08-09 11:47:09 -0400

    Move to lib + bin/ layout

---
+++ src/bin/diff.rs
@@ -0,0 +1,8 @@
+extern crate adler32;
+extern crate env_logger;
+#[macro_use] extern crate log;
+extern crate rs_sync;
+extern crate sha1;
+
+fn main() {
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 extern crate adler32;
-extern crate env_logger;
 #[macro_use] extern crate log;
 extern crate sha1;
 
 mod hasher;
-
-fn main() {
-}
